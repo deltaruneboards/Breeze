158f6476e3f4af0bd8e76a55f3030e49ebdd26e6
Luka Simić <git@kocka.tech>
2026-09-08 03:55:08 +0200

    Remove loading flash.

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,12 @@
 import type { ReactElement } from "react";
 
 export default function Loading(): ReactElement {
+	/* DUMB change: we don't need this
 	return (
 		<div className="loading" data-testid="loading">
 			&#8230;
 		</div>
 	);
+	*/
+	return <></>;
 }
